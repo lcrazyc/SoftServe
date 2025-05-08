aa0f4712ac38dd264efd438f241bbca663e081e2
DanielaChokan <144257212+DanielaChokan@users.noreply.github.com>
2025-05-09 01:04:51 +0300

    Create footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,16 @@
+import './footer.css';
+
+const Footer = () =>  {
+  return (
+    <footer className="footer">
+        <div className="logo-circle" />
+        <div className="footer-icons">
+          <img src="./Icons/Mail.svg" alt="Save" />
+          <img src="./Icons/Facebook.svg" alt="Account" />
+          <img src="./Icons/Phone.svg" alt="Menu" />
+        </div>
+      </footer>
+  );
+}
+
+export default Footer;
