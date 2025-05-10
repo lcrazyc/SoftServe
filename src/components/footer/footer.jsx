da144903470de8055df6886e4c81d3c8804f367f
sofiia <kr23s02@gmail.com>
2025-05-10 18:36:32 +0300

    Update footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,11 +3,19 @@ import './footer.css';
 const Footer = () =>  {
   return (
     <footer className="footer">
-        <div className="logo-circle" />
+        <div className="logo-circle">
+          <img src="./Icons/logo.svg" alt="logo" />
+          </div>
         <div className="footer-icons">
-          <img src="./Icons/Mail.svg" alt="Save" />
-          <img src="./Icons/Facebook.svg" alt="Account" />
-          <img src="./Icons/Phone.svg" alt="Menu" />
+          <a href="mailto:example@example.com">
+            <img src="./Icons/Mail.svg" alt="Email" className="icon"/>
+          </a>
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <img src="./Icons/Facebook.svg" alt="Facebook" className="icon"/>
+          </a>
+          <a href="tel:+1234567890">
+            <img src="./Icons/Phone.svg" alt="Phone" className="icon"/>
+          </a>
         </div>
       </footer>
   );
